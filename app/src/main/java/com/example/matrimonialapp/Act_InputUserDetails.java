5fc310c47cc1114736ea1e7bc2f9c681d58cd1d7
NafiuNoman <nafiunoman@gmail.com>
2020-12-13 12:10:07 +0600

    Implementing MDC

--- app/src/main/java/com/example/matrimonialapp/Act_InputUserDetails.java
+++ app/src/main/java/com/example/matrimonialapp/Act_InputUserDetails.java
@@ -2,16 +2,20 @@ package com.example.matrimonialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,13 +23,21 @@ import com.google.firebase.database.FirebaseDatabase;
 public class Act_InputUserDetails extends AppCompatActivity {
 
 
-    TextInputLayout name,creator;
+    TextInputLayout name,creator,dob,age;
 
-    AutoCompleteTextView religion,autoCreator,autoMaritalStatus;
+    TextInputEditText dobInput;
+
+
+    AutoCompleteTextView
+            autoReligion,
+            autoCreator,
+            autoMaritalStatus,
+            autolivingStatus,
+            autoWorkingStatus,
+            autoPhysicalStatus;
 
     RadioGroup genderRadio;
 
-    EditText age;
 
     Button btn;
 
@@ -33,6 +45,13 @@ public class Act_InputUserDetails extends AppCompatActivity {
     String[] religionNames;
     String[] creatorsName;
     String[] maritalStatus;
+    String[] physicalStatus;
+    String[] livingStatus;
+    String[] workingStatus;
+
+
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
 
 
 
@@ -45,27 +64,43 @@ public class Act_InputUserDetails extends AppCompatActivity {
 
         name = findViewById(R.id.IdInputName);
         creator = findViewById(R.id.IdInputCreator);
+        dob = findViewById(R.id.IdInputDOB);
+        dobInput = findViewById(R.id.IdInputDobInput);
 
+        autoReligion = findViewById(R.id.IdInputReligion);
         autoCreator = findViewById(R.id.IdInputAutoCreator);
         autoMaritalStatus = findViewById(R.id.IdInputAutoMaritalStatus);
+        autoPhysicalStatus = findViewById(R.id.IdInputPhysicalStatus);
+        autolivingStatus = findViewById(R.id.IdInputLiving);
+        autoWorkingStatus=findViewById(R.id.IdInputWorking);
+
+
         age = findViewById(R.id.IdInputAge);
-        religion = findViewById(R.id.IdInputReligion);
         genderRadio = findViewById(R.id.IdInputRadioGroupGender);
 
-        btn  = findViewById(R.id.IdInputbtn);
+        btn  = findViewById(R.id.IdInputBtn);
 
 
         religionNames = getResources().getStringArray(R.array.Religions);
         creatorsName = getResources().getStringArray(R.array.Creator);
         maritalStatus = getResources().getStringArray(R.array.maritalStatus);
+        livingStatus = getResources().getStringArray(R.array.livingStatus);
+        workingStatus = getResources().getStringArray(R.array.workStatus);
+        physicalStatus = getResources().getStringArray(R.array.physicalCondition);
 
         ArrayAdapter<String> religionAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,religionNames);
         ArrayAdapter<String> creatorAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,creatorsName);
         ArrayAdapter<String> maritalAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,maritalStatus);
+        ArrayAdapter<String> workingAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,workingStatus);
+        ArrayAdapter<String> livingAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,livingStatus);
+        ArrayAdapter<String> physicalAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,physicalStatus);
 
-        religion.setAdapter(religionAdapter);
+        autoReligion.setAdapter(religionAdapter);
         autoCreator.setAdapter(creatorAdapter);
         autoMaritalStatus.setAdapter(maritalAdapter);
+        autolivingStatus.setAdapter(livingAdapter);
+        autoWorkingStatus.setAdapter(workingAdapter);
+        autoPhysicalStatus.setAdapter(physicalAdapter);
 
 
     }
@@ -77,8 +112,8 @@ public class Act_InputUserDetails extends AppCompatActivity {
 
         sName = name.getEditText().getText().toString().trim();
 
-        sAge = age.getText().toString().trim();
-        sReligion =religion.getText().toString().trim();
+        sAge = age.getEditText().getText().toString().trim();
+        sReligion = autoReligion.getText().toString().trim();
 
         
         ClsUserDetails clsUserDetails = new ClsUserDetails(sName,sAge,sGender,sReligion);
@@ -110,6 +145,27 @@ public class Act_InputUserDetails extends AppCompatActivity {
                 Toast.makeText(this, "Male", Toast.LENGTH_SHORT).show();
 
         }
+
+
+    }
+
+    public void ClickedDateOfBirth(View view) {
+
+        calendar = Calendar.getInstance();
+
+        int day =calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        datePickerDialog =    new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int iYear, int iMonth, int iDay) {
+
+               dobInput.setText(""+iDay+"/"+(iMonth+1)+"/"+iYear);
+            }
+        },day,month,year);
+
+        datePickerDialog.show();
 
 
     }
