73ce716301a376eb2287e1532d6a88174312d584
NafiuNoman <nafiunoman@gmail.com>
2020-12-14 09:30:14 +0600

    Implementing MDC

--- app/src/main/java/com/example/matrimonialapp/Act_InputUserDetails.java
+++ app/src/main/java/com/example/matrimonialapp/Act_InputUserDetails.java
@@ -9,9 +9,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -23,16 +21,16 @@ import com.google.firebase.database.FirebaseDatabase;
 public class Act_InputUserDetails extends AppCompatActivity {
 
 
-    TextInputLayout name,creator,dob,age;
+    TextInputLayout name, creator, dob, age;
 
-    TextInputEditText dobInput;
+    TextInputEditText dobInput,height,income,cAddress,pAddress;
 
 
     AutoCompleteTextView
             autoReligion,
             autoCreator,
             autoMaritalStatus,
-            autolivingStatus,
+            autoLivingStatus,
             autoWorkingStatus,
             autoPhysicalStatus;
 
@@ -41,7 +39,8 @@ public class Act_InputUserDetails extends AppCompatActivity {
 
     Button btn;
 
-    String sName,sAge,sGender,sReligion;
+    String sCreator, sName, sAge, sGender, sDob, sMaritalStatus, sReligion, sHeight,
+            sPhysicalStatus, sLivingStatus, sIncome, sPaddress, sCaddress, sWorkingStatus;
     String[] religionNames;
     String[] creatorsName;
     String[] maritalStatus;
@@ -54,9 +53,6 @@ public class Act_InputUserDetails extends AppCompatActivity {
     DatePickerDialog datePickerDialog;
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,20 +61,24 @@ public class Act_InputUserDetails extends AppCompatActivity {
         name = findViewById(R.id.IdInputName);
         creator = findViewById(R.id.IdInputCreator);
         dob = findViewById(R.id.IdInputDOB);
+        height = findViewById(R.id.IdInputHeight);
         dobInput = findViewById(R.id.IdInputDobInput);
+        income = findViewById(R.id.IdInputIncome);
+        cAddress = findViewById(R.id.IdInputCurrentAddress);
+        pAddress = findViewById(R.id.IdInputPermanentAddress);
 
         autoReligion = findViewById(R.id.IdInputReligion);
         autoCreator = findViewById(R.id.IdInputAutoCreator);
         autoMaritalStatus = findViewById(R.id.IdInputAutoMaritalStatus);
         autoPhysicalStatus = findViewById(R.id.IdInputPhysicalStatus);
-        autolivingStatus = findViewById(R.id.IdInputLiving);
-        autoWorkingStatus=findViewById(R.id.IdInputWorking);
+        autoLivingStatus = findViewById(R.id.IdInputLiving);
+        autoWorkingStatus = findViewById(R.id.IdInputWorking);
 
 
         age = findViewById(R.id.IdInputAge);
         genderRadio = findViewById(R.id.IdInputRadioGroupGender);
 
-        btn  = findViewById(R.id.IdInputBtn);
+        btn = findViewById(R.id.IdInputBtn);
 
 
         religionNames = getResources().getStringArray(R.array.Religions);
@@ -88,17 +88,17 @@ public class Act_InputUserDetails extends AppCompatActivity {
         workingStatus = getResources().getStringArray(R.array.workStatus);
         physicalStatus = getResources().getStringArray(R.array.physicalCondition);
 
-        ArrayAdapter<String> religionAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,religionNames);
-        ArrayAdapter<String> creatorAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,creatorsName);
-        ArrayAdapter<String> maritalAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,maritalStatus);
-        ArrayAdapter<String> workingAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,workingStatus);
-        ArrayAdapter<String> livingAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,livingStatus);
-        ArrayAdapter<String> physicalAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,physicalStatus);
+        ArrayAdapter<String> religionAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, religionNames);
+        ArrayAdapter<String> creatorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, creatorsName);
+        ArrayAdapter<String> maritalAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, maritalStatus);
+        ArrayAdapter<String> workingAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, workingStatus);
+        ArrayAdapter<String> livingAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, livingStatus);
+        ArrayAdapter<String> physicalAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, physicalStatus);
 
         autoReligion.setAdapter(religionAdapter);
         autoCreator.setAdapter(creatorAdapter);
         autoMaritalStatus.setAdapter(maritalAdapter);
-        autolivingStatus.setAdapter(livingAdapter);
+        autoLivingStatus.setAdapter(livingAdapter);
         autoWorkingStatus.setAdapter(workingAdapter);
         autoPhysicalStatus.setAdapter(physicalAdapter);
 
@@ -106,27 +106,32 @@ public class Act_InputUserDetails extends AppCompatActivity {
     }
 
     public void sendToRealDatabase(View view) {
-        
-        
-        
 
+
+        sCreator = creator.getEditText().getText().toString().trim();
         sName = name.getEditText().getText().toString().trim();
-
         sAge = age.getEditText().getText().toString().trim();
         sReligion = autoReligion.getText().toString().trim();
+        sDob = dobInput.getText().toString().trim();
+        sMaritalStatus = autoMaritalStatus.getText().toString().trim();
+        sReligion = autoReligion.getText().toString().trim();
+        sHeight = height.getText().toString().trim();
+        sPhysicalStatus = autoPhysicalStatus.getText().toString().trim();
+        sLivingStatus = autoLivingStatus.getText().toString().trim();
+        sIncome = income.getText().toString().trim();
+        sCaddress = cAddress.getText().toString().trim();
+        sPaddress = pAddress.getText().toString().trim();
+        sWorkingStatus = autoWorkingStatus.getText().toString().trim();
 
-        
-        ClsUserDetails clsUserDetails = new ClsUserDetails(sName,sAge,sGender,sReligion);
-        
+
+        ClsUserDetails clsUserDetails = new ClsUserDetails(sCreator, sName, sGender, sAge, sDob, sMaritalStatus, sReligion,
+                sHeight, sPhysicalStatus, sLivingStatus, sIncome, sPaddress, sCaddress, sWorkingStatus);
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference reference = database.getReference("student");
         reference.child(sAge).setValue(clsUserDetails);
 
         Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show();
-        
-
-
-
 
 
     }
@@ -134,13 +139,12 @@ public class Act_InputUserDetails extends AppCompatActivity {
     public void GenderClicked(View view) {
 
 
-        switch (view.getId())
-        {
+        switch (view.getId()) {
             case R.id.IdRadioButtonMale:
                 sGender = "Male";
 
             case R.id.IdRadioButtonFemale:
-                sGender="Female";
+                sGender = "Female";
             default:
                 Toast.makeText(this, "Male", Toast.LENGTH_SHORT).show();
 
@@ -153,17 +157,17 @@ public class Act_InputUserDetails extends AppCompatActivity {
 
         calendar = Calendar.getInstance();
 
-        int day =calendar.get(Calendar.DAY_OF_MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
         int month = calendar.get(Calendar.MONTH);
         int year = calendar.get(Calendar.YEAR);
 
-        datePickerDialog =    new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int iYear, int iMonth, int iDay) {
 
-               dobInput.setText(""+iDay+"/"+(iMonth+1)+"/"+iYear);
+                dobInput.setText("" + iDay + "/" + (iMonth + 1) + "/" + iYear);
             }
-        },day,month,year);
+        }, day, month, year);
 
         datePickerDialog.show();
 
--- app/src/main/java/com/example/matrimonialapp/ClsUserDetails.java
+++ app/src/main/java/com/example/matrimonialapp/ClsUserDetails.java
@@ -2,49 +2,136 @@ package com.example.matrimonialapp;
 
 public class ClsUserDetails {
 
+String creator,name,gender,age,dateOfBirth,maritalStatus,
+        religion,height,physicalStatus,livingStatus,income,
+        pAddress,cAddress,workingStatus;
 
-    String Name,Age,Gender,Religion;
-
-    public ClsUserDetails(String name, String age, String gender, String religion) {
-        Name = name;
-        Age = age;
-        Gender = gender;
-        Religion = religion;
+    public ClsUserDetails(String creator, String name, String gender, String age, String dateOfBirth, String maritalStatus, String religion, String height, String physicalStatus, String livingStatus, String income, String pAddress, String cAddress, String workingStatus) {
+        this.creator = creator;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.maritalStatus = maritalStatus;
+        this.religion = religion;
+        this.height = height;
+        this.physicalStatus = physicalStatus;
+        this.livingStatus = livingStatus;
+        this.income = income;
+        this.pAddress = pAddress;
+        this.cAddress = cAddress;
+        this.workingStatus = workingStatus;
     }
 
+    public String getCreator() {
+        return creator;
+    }
 
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
-    }
-
-    public String getAge() {
-        return Age;
-    }
-
-    public void setAge(String age) {
-        Age = age;
+        this.name = name;
     }
 
     public String getGender() {
-        return Gender;
+        return gender;
     }
 
     public void setGender(String gender) {
-        Gender = gender;
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
     }
 
     public String getReligion() {
-        return Religion;
+        return religion;
     }
 
     public void setReligion(String religion) {
-        Religion = religion;
+        this.religion = religion;
     }
 
+    public String getHeight() {
+        return height;
+    }
 
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getPhysicalStatus() {
+        return physicalStatus;
+    }
+
+    public void setPhysicalStatus(String physicalStatus) {
+        this.physicalStatus = physicalStatus;
+    }
+
+    public String getLivingStatus() {
+        return livingStatus;
+    }
+
+    public void setLivingStatus(String livingStatus) {
+        this.livingStatus = livingStatus;
+    }
+
+    public String getIncome() {
+        return income;
+    }
+
+    public void setIncome(String income) {
+        this.income = income;
+    }
+
+    public String getpAddress() {
+        return pAddress;
+    }
+
+    public void setpAddress(String pAddress) {
+        this.pAddress = pAddress;
+    }
+
+    public String getcAddress() {
+        return cAddress;
+    }
+
+    public void setcAddress(String cAddress) {
+        this.cAddress = cAddress;
+    }
+
+    public String getWorkingStatus() {
+        return workingStatus;
+    }
+
+    public void setWorkingStatus(String workingStatus) {
+        this.workingStatus = workingStatus;
+    }
 }
